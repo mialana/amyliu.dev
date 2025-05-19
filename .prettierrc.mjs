bdfce147dccaf80be55a3ebffea8f61ea9e5c5db
mialana <96770662+mialana@users.noreply.github.com>
2025-05-18 21:22:03 -0400

    Add astro prettier plugin and lint

---
+++ .prettierrc.mjs
@@ -0,0 +1,35 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: ["**/*.astro"],
+            excludeFiles: ["**/*.json"],
+            options: {
+                parser: "astro",
+                printWidth: 80,
+                tabWidth: 4,
+                proseWrap: "preserve",
+            },
+        },
+        {
+            files: ["**/*.*js"],
+            options: {
+                parser: "typescript",
+                printWidth: 80,
+                tabWidth: 4,
+                proseWrap: "preserve",
+            },
+        },
+        {
+            files: ["**/*.*json"],
+            options: {
+                parser: "json",
+                printWidth: 80,
+                tabWidth: 4,
+                proseWrap: "preserve",
+            },
+        },
+    ],
+};
