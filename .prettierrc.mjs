b6bad9a78d4209d8495267e95b62cb4769f63315
mialana <96770662+mialana@users.noreply.github.com>
2025-05-22 15:29:15 -0400

    Add dependabot, optimize prettier behavior

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,35 +1,11 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
+
 export default {
     plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-    overrides: [
-        {
-            files: ["**/*.astro"],
-            excludeFiles: ["**/*.json"],
-            options: {
-                parser: "astro",
-                printWidth: 80,
-                tabWidth: 4,
-                proseWrap: "preserve",
-            },
-        },
-        {
-            files: ["**/*.*js", "**/*.ts*"],
-            options: {
-                parser: "typescript",
-                printWidth: 80,
-                tabWidth: 4,
-                proseWrap: "preserve",
-            },
-        },
-        {
-            files: ["**/*.*json"],
-            options: {
-                parser: "json",
-                printWidth: 80,
-                tabWidth: 4,
-                proseWrap: "preserve",
-            },
-        },
-    ],
+
+    // Global default options for all files
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
 };
