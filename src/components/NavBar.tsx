c996115a582684e5e85bf1df18893bcff54350bf
mialana <96770662+mialana@users.noreply.github.com>
2025-05-24 11:51:33 -0400

    Add "under construction" UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -86,12 +86,12 @@ export default function NavBar() {
                         <li>
                             <a href="/#landing">Landing</a>
                         </li>
-                        <li>
+                        {/*<li>
                             <a href="/#about">About</a>
                         </li>
                         <li>
                             <a href="/#projects">Projects</a>
-                        </li>
+                        </li>*/}
                     </ul>
                 </div>
             </nav>
