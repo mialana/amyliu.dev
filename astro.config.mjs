7f41e0cb645923481aa2bd349eef4efa8835fd0d
mialana <96770662+mialana@users.noreply.github.com>
2025-05-30 20:40:11 -0400

    Add rehypeMermaid, edit GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,25 @@ import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
+import rehypeMermaid from "rehype-mermaid";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://amyliu.dev/",
     integrations: [react(), sitemap(), mdx()],
     vite: { plugins: [tailwindcss()] },
     devToolbar: { enabled: false },
+    markdown: {
+        syntaxHighlight: { type: "shiki", excludeLangs: ["mermaid"] },
+        rehypePlugins: [
+            [
+                rehypeMermaid,
+                {
+                    strategy: "img-svg",
+
+                    mermaidConfig: { theme: "default" },
+                },
+            ],
+        ],
+    },
 });
