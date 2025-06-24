7294cdbdb020c5efddea1f45c59d7654849de931
mialana <96770662+mialana@users.noreply.github.com>
2025-06-24 13:56:42 -0400

    Small styling changes project slug

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -13,7 +13,13 @@ export default function Toast() {
         <div
             className={`pointer-none absolute top-2 right-2 z-100 w-fit max-w-screen rounded-sm bg-black px-8 py-4 text-lg text-white transition-opacity duration-500 ease-linear ${visible ? "visible" : "invisible"}`}
         >
-            <strong>NOTE:</strong> This site is a work-in-progress.
+            <div>
+                <strong>NOTE: </strong>This site is currently a
+                work-in-progress.
+            </div>
+            <div>
+                <strong>LAST UPDATE: </strong>June 24, 2025
+            </div>
             <div
                 className={`absolute right-0 bottom-0 m-[2px] h-1 rounded-sm bg-white transition-[width] duration-[5000ms] ease-linear ${visible ? "visible w-0" : "invisible w-[calc(100%-4px)]"}`}
             />
