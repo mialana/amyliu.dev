34edbb4c9e962fde487f9676bf587e0f45dc8779
mialana <96770662+mialana@users.noreply.github.com>
2025-08-25 09:43:28 -0700

    Fix trailing slash inconsistencies

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -169,7 +169,7 @@ export default function Search() {
                 onChange={setTypeFilter}
                 options={typeOptions}
             />
-            <a href="/tags" className="text-[10px] underline">
+            <a href="/tags/" className="text-[10px] underline">
                 View Tags Index
             </a>
         </section>
