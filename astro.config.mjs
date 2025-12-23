3a892addbdbab58282e6d654329c9a12330fc12a
Amy Liu <liu.amy05@gmail.com>
2025-12-22 17:12:29 -0800

    ci: fix vite version inconsistency for astro, tailwind, and react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,41 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
-
-import mdx from "@astrojs/mdx";
 
 import remarkMath from "remark-math";
 import rehypeMathJaxChtml from "rehype-mathjax/chtml";
 
 import astroExpressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginCodeCaption } from "@fujocoded/expressive-code-caption";
+
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
+/**
+ * @returns {import("expressive-code").ExpressiveCodePlugin}
+ */
+export function codeCaption() {
+    return /** @type {import("expressive-code").ExpressiveCodePlugin} */ (
+        pluginCodeCaption()
+    );
+}
 
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
 const expressiveCodeConfig = {
-    plugins: [pluginLineNumbers()],
+    plugins: [pluginLineNumbers(), pluginCollapsibleSections(), codeCaption()],
     shiki: { langAlias: { usda: "bash" } },
     styleOverrides: { borderRadius: "0.375rem", codePaddingInline: "0.5rem" },
     themes: ["gruvbox-dark-hard"],
     minSyntaxHighlightingColorContrast: 7.5,
-    defaultProps: { wrap: true, showLineNumbers: true },
+    defaultProps: {
+        wrap: true,
+        showLineNumbers: true,
+        collapseStyle: "collapsible-auto",
+    },
 };
 
 // https://astro.build/config
@@ -30,10 +43,9 @@ export default defineConfig({
     server: { host: "0.0.0.0" },
     site: "https://amyliu.dev/",
     integrations: [
-        react(),
         sitemap(),
         astroExpressiveCode(expressiveCodeConfig),
-        mdx(),
+        react(),
     ],
     vite: { plugins: [tailwindcss()] },
     devToolbar: { enabled: false },
