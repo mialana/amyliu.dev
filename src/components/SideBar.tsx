c7fd413d6373bbf852b0be2902af7d005f0cb1ec
mialana <96770662+mialana@users.noreply.github.com>
2026-01-02 02:52:14 -0800

    feat: solve layout switching between mobile and desktop

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,27 +1,31 @@
-import { useState, useEffect, useLayoutEffect } from "react";
-import { hasSlotReact } from "@/lib/hasSlot";
-import { type SidebarType } from "@/lib/handleSideBarState";
+import { useState, useEffect } from "react";
+import { type SideBarType, checkIfMobile } from "@/lib/handleSideBarState";
 
 interface SideBarProps {
-    type: SidebarType;
+    sideBarType: SideBarType;
     active: boolean;
-    children?: any;
+    children?: React.ReactNode;
 }
 
-export default function SideBar({ type, active, children }: SideBarProps) {
+export function MobileSideBar({ sideBarType, active, children }: SideBarProps) {
+    return <div className={`relative overflow-clip`}>{children}</div>;
+}
+
+export function DesktopSideBar({ sideBarType, active, children }: SideBarProps) {
     const [open, setOpen] = useState(false);
 
     useEffect(() => {
-        if (!active || !hasSlotReact(children)) return; /* stay hidden if not active or valid */
+        if (!active) return; /* stay hidden if not active or valid */
 
         const app = document.getElementById("app");
         if (!app) return;
 
         /* set up mutation observer for `data-${type}state` to track open state */
-        const attrName = `data-${type}state`;
+        const attrName = `data-${sideBarType}state`;
 
         // initialize state on load
         setOpen(app.getAttribute(attrName) === "open");
+
         const observer = new MutationObserver((mutations) => {
             for (const m of mutations) {
                 if (m.type === "attributes" && m.attributeName === attrName) {
@@ -29,24 +33,32 @@ export default function SideBar({ type, active, children }: SideBarProps) {
                 }
             }
         });
-
         observer.observe(app, { attributes: true, attributeFilter: [attrName] });
-    }, []);
+        return () => observer.disconnect();
+    }, [sideBarType, active]);
 
     return (
-        <div
-            className={`absolute w-screen transition-[max-width] duration-1000 md:relative md:w-auto ${open ? "z-20 max-w-screen md:max-w-[40vw]" : "z-5 max-w-0"} bg-primary-neutral ${type == "nav" ? "left-0 md:rounded-r-sm" : "right-0 md:rounded-l-sm"} h-full overflow-x-visible pt-8 pb-4 shadow-lg`}
-            onClick={() => {
-                if (window.innerWidth <= 768) {
-                    setOpen(false);
-                }
-            }}
+        <aside
+            className={`h-full overflow-x-clip overflow-y-scroll transition-[max-width] ${open ? "max-w-sm" : "max-w-0"}`}
         >
-            <div
-                className={`mx-4 h-full overflow-x-scroll md:max-w-[30vw] ${open ? "" : "opacity-0"} transition-opacity duration-500`}
-            >
-                <div className="size-full">{children}</div>
-            </div>
-        </div>
+            {children}
+        </aside>
     );
+}
+
+export default function SideBar(props: SideBarProps) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const onResize = () => {
+            setIsMobile(checkIfMobile());
+        };
+
+        onResize(); // set up initial state
+        window.addEventListener("resize", onResize);
+
+        return () => window.removeEventListener("resize", onResize);
+    }, []);
+
+    return isMobile ? <MobileSideBar {...props} /> : <DesktopSideBar {...props} />;
 }
--- src/lib/handleSideBarState.ts
+++ src/lib/handleSideBarState.ts
@@ -1,4 +1,9 @@
-export type SidebarType = "nav" | "aside";
+export type SideBarType = "nav" | "aside";
+
+export const checkIfMobile = () =>
+    window.matchMedia(
+        `(max-width: ${getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-desktop").trim()})`,
+    ).matches;
 
 function toggleSideBarStyle(button: HTMLButtonElement) {
     const toggledClasses = ["rotate-180", "brightness-50", "brightness-125"];
@@ -8,7 +13,7 @@ function toggleSideBarStyle(button: HTMLButtonElement) {
     });
 }
 
-function handleSideBarButtonClicked(button: HTMLButtonElement, sidebarType: SidebarType) {
+function handleSideBarButtonClicked(button: HTMLButtonElement, sidebarType: SideBarType) {
     const app = document.getElementById("app");
     if (!app) return;
 
@@ -19,39 +24,32 @@ function handleSideBarButtonClicked(button: HTMLButtonElement, sidebarType: Side
 }
 
 export function handleSideBarButtonState() {
-    window.addEventListener("DOMContentLoaded", () => {
-        // if we are below the 'desktop' breakpoint, i.e. mobile, return
-        if (
-            !window.matchMedia(
-                `(min-width: ${window.getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-desktop").trim()})`,
-            )
-        )
-            return;
+    // if we are below the 'desktop' breakpoint, i.e. mobile, return
+    if (checkIfMobile()) return;
 
-        const app = document.getElementById("app");
-        if (!app) return;
+    const app = document.getElementById("app");
+    if (!app) return;
 
-        const layout = app.dataset.layout;
-        if (!layout) return;
+    const layout = app.dataset.layout;
+    if (!layout) return;
 
-        const isActiveMap = new Map<SidebarType, boolean>([
-            ["nav", layout === "BOTH" || layout === "NO_ASIDE"],
-            ["aside", layout === "BOTH" || layout === "NO_NAV"],
-        ]);
+    const isActiveMap = new Map<SideBarType, boolean>([
+        ["nav", layout === "BOTH" || layout === "NO_ASIDE"],
+        ["aside", layout === "BOTH" || layout === "NO_NAV"],
+    ]);
 
-        isActiveMap.forEach((active, sidebarType) => {
-            if (!active) return;
-            const button = document.getElementById(`${sidebarType}-sidebar-button`);
+    isActiveMap.forEach((active, sidebarType) => {
+        if (!active) return;
+        const button = document.getElementById(`${sidebarType}-sidebar-button`);
 
-            if (!(button instanceof HTMLButtonElement)) return;
+        if (!(button instanceof HTMLButtonElement)) return;
 
-            button.classList.toggle("desktop:block"); /* make visible if active */
-            toggleSideBarStyle(button); // make the button use its 'open' style
+        button.classList.toggle("desktop:block"); /* make visible if active */
+        toggleSideBarStyle(button); // make the button use its 'open' style
 
-            /* set up click event listener */
-            button?.addEventListener("click", (event) => {
-                if (event.currentTarget) handleSideBarButtonClicked(button, sidebarType);
-            });
+        /* set up click event listener */
+        button?.addEventListener("click", (event) => {
+            if (event.currentTarget) handleSideBarButtonClicked(button, sidebarType);
         });
     });
 }
